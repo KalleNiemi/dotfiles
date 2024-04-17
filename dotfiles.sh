4adbc318d0417cca0305898f5602d539cf2cdf0c
Kalle Niemi <KalleNiemi@protonmail.com>
2024-04-17 20:05:21 +0300

    Fixed dotfiles commands
    Alias does not work like that, this is quickfix

--- dotfiles.sh
+++ dotfiles.sh
@@ -28,11 +28,13 @@ get_package vim
 get_package tmux
 #tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
+sleep 3
 echo 'alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'' >> ~/.bashrc
 git clone --bare https://github.com/KalleNiemi/dotfiles.git $HOME/.dotfiles
+sleep 2
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
+sleep 1
+/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" checkout
+/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" config --local status.showUntrackedFiles no
 
 echo 'export EDITOR=vim' >> ~/.bashrc
